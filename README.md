bdeb12ec28e42a5007b0a19ae6df7da83b0dd018
Jason Darby <jasonkdarby@gmail.com>
2016-05-29 22:28:23 -0400

    added a higher level concept for a full run

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #Test Result Model
+I need a common model for test results, so: this.
 
 To get it as a dependency (for now) you can add my [bintray repository](https://bintray.com/jasonkdarby/generic/test-result-model/view).
 via gradle:
@@ -12,4 +13,16 @@ dependencies {
 }
 ```
 
-I need a common model for test results, so: this.
+##Model
+
+###Run
+A run corresponds to a group of tests that would fully describe the state of an application/library.  It is equivalent to a full Jenkins pipeline (the testing aspects).
+
+###Test
+A test simultaneously corresponds to a whole collection of the high level concept of a test (unit, integration, functional, system, acceptance, what have you) as well as individual instances of those tests and components of those tests where it makes sense.
+
+###Details
+Details describe the components of a step, either tests or logs that describe the test.
+
+###LogMessage
+A log message is used to document the actions and/or intermediate results of a test.  In Spock you might use the block labels as log messages.
--- src/main/kotlin/com/jdarb/testresult/model/TestResultModel.kt
+++ src/main/kotlin/com/jdarb/testresult/model/TestResultModel.kt
@@ -1,17 +1,42 @@
 package com.jdarb.testresult.model
 
+import java.time.Duration
 import java.time.Instant
+import java.util.UUID
+
+data class Run(
+        val name: String,
+        val startTime: Instant,
+        val endTime: Instant,
+        val tests: List<Test> = emptyList(),
+        val id: String = UUID.randomUUID().toString()
+)
+
+fun Run.duration() = Duration.between(startTime, endTime)
 
 enum class State {
     PASS, FAIL, SKIP
 }
 
-data class Test(val name: String, val status: State, val details: Details)
+data class Test(
+        val name: String,
+        val description: String,
+        val startTime: Instant,
+        val endTime: Instant,
+        val status: State,
+        val details: Details = noDetails()
+)
 
-data class Details(val logMessages: List<LogMessage>, val childTests: List<Test>)
+fun Test.duration() = Duration.between(startTime, endTime)
 
-enum class MessageType {
-    DOCUMENTATION, INSTANCE
-}
+data class Details(
+        val logMessages: List<LogMessage> = emptyList(),
+        val childTests: List<Test> = emptyList()
+)
 
-data class LogMessage(val time: Instant, val messageType: MessageType, val text: String)
+fun noDetails() = Details()
+
+data class LogMessage(
+        val text: String,
+        val instant: Instant = Instant.now()
+)
